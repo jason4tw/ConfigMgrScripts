88dcde0bd3530efea2d57581b2733ee4e17696d0
Jason Sandys <jason@sandys.us>
2019-10-23 17:42:14 -0500

    Update Rename-CMCollections.ps1
    1.1 (23 October 2018): Added insert prefix option, fixed parameter sets, added progress bar

--- Other/Rename-CMCollections.ps1
+++ Other/Rename-CMCollections.ps1
@@ -22,35 +22,44 @@
         ...
 
     .NOTES
-        Version 1.0
+        Version 1.1
         Jason Sandys
 
         Version History
+        - 1.1 (23 October 2018): Added insert prefix option, fixed parameter sets, added progress bar
         - 1.0 (27 September 2018): Initial Version
 
         Limitations and Issues
        
 #>
 
-[CmdletBinding(DefaultParameterSetName = 'Standard')]
+[CmdletBinding()]
 param
 (
-    [Parameter(Mandatory=$False, HelpMessage = 'The collection(s) to rename (PowerShell wildcards are allowed)', ParameterSetName='Standard')]
-    [Parameter(ParameterSetName='FolderFilter')]
-    [string]$Name = '*',
-    [Parameter(Mandatory=$True, HelpMessage = 'The regular expression search string.', ParameterSetName='Standard')]
-    [Parameter(ParameterSetName='FolderFilter')]
-    [string]$Search,
-    [Parameter(Mandatory=$True, HelpMessage = 'The regular expression replace string.', ParameterSetName='Standard')]
-    [Parameter(ParameterSetName='FolderFilter')]
-    [string]$Replace,
-    [Parameter(Mandatory=$True, HelpMessage = 'The folder to rename collections in.', ParameterSetName='FolderFilter')]
-    [string]$Folder,
-    [Parameter(Mandatory=$False, HelpMessage = 'Recurse sub-folders.', ParameterSetName='FolderFilter')]
-    [switch]$Recurse,
-    [Parameter(Mandatory=$False, HelpMessage = 'The site server to connect to.', ParameterSetName='FolderFilter')]
-    [string]$SiteServer = $env:computername,
-    [switch]$WhatIf
+    [Parameter(Mandatory=$False, HelpMessage = 'The site server to connect to.')]
+    [string] $SiteServer = $env:computername,
+    
+    [Parameter(Mandatory=$False, HelpMessage = 'The collection(s) to rename (PowerShell wildcards are allowed)')]
+    [alias('Name')]
+    [alias('NameFilter')]
+    [alias('Collection')]
+    [string] $CollectionNameFilter = '*',
+    
+    [Parameter(Mandatory=$True, HelpMessage = 'The folder to rename collections in.')]
+    [alias('Folder')]
+    [string] $FolderName,
+    [Parameter(Mandatory=$False, HelpMessage = 'Recurse sub-folders.')]
+    [switch] $Recurse,
+    
+    [Parameter(Mandatory=$True, HelpMessage = 'The regular expression search string.', ParameterSetName='Replace')]
+    [string] $Search,
+    [Parameter(Mandatory=$True, HelpMessage = 'The regular expression replace string.', ParameterSetName='Replace')]
+    [string] $Replace,
+
+    [Parameter(Mandatory=$True, HelpMessage = 'The prefix to insert at the beginning of the folder name.', ParameterSetName='Prefix')]
+    [string] $Prefix,
+
+    [switch] $WhatIf
 )
 
 function Process-DeviceCollectionsInFolder
@@ -58,31 +67,64 @@ function Process-DeviceCollectionsInFolder
     param
     (
 	    [Parameter(Mandatory=$True)]
-        [string]$FolderNodeID,
+        [string] $FolderNodeID,
         [Parameter(Mandatory=$True)]
-        [string]$FolderNodeName
+        [string] $FolderNodeName,
+        [switch] $InsertPrefix,
+        [switch] $ReplaceString
     )
 
-    $memberCollections = Get-WMIObject -Computer $providerSystem -Namespace $providerNamespace -Class SMS_ObjectContainerItem `
+    $folderCollections = Get-WMIObject -Computer $providerSystem -Namespace $providerNamespace -Class SMS_ObjectContainerItem `
         -Filter "ContainerNodeID='$FolderNodeID' And ObjectType='5000'"
 
-    foreach($collectionContainerItem in $memberCollections)
+    if($WhatIf -eq $true)
+    {
+        Write-Host -ForegroundColor Blue -BackgroundColor White "`nWhatIf Mode - No changes will be made"  
+    }
+    
+    Write-Host "`nProcessing collections in $FolderNodeName ..."
+
+    $collectionCounter = 0
+    $collectionCount = $folderCollections.Count
+    Write-Progress -Id 1 -Activity "Processing collections in $FolderNodeName" -Status "0 of $collectionCount"
+
+    foreach($collectionContainerItem in $folderCollections)
     {
         $collection = Get-CMDeviceCollection -CollectionId $collectionContainerItem.InstanceKey
+        $collectioncounter++
  
-        if($collection.Name -like "$Name")
+        Write-Progress -Id 1 -Activity "Processing collections in $FolderNodeName" -Status "$collectionCounter of $($folderCollections.Count)" -CurrentOperation $collection.Name -PercentComplete (($collectionCounter / $collectionCount) * 100)
+
+        if($collection.Name -like "$CollectionNameFilter")
         {
-            $newName = $collection.name -replace $Search, $Replace
-
-            Write-Host "Renaming `"$($collection.name)`" to `"$newName`""
-
-            if($WhatIf -eq $false)
+            if ($ReplaceString)
             {
-                Set-CMDeviceCollection -CollectionId $collection.CollectionId -NewName "$newName"
+                $newName = $collection.name -replace $Search, $Replace
+            }
+            elseif ($InsertPrefix) 
+            {
+                $newName = "$Prefix" + $collection.Name
+            }
+            else
+            {
+                $newName = ''
+            }
+
+            if($newName -ne '')
+            {
+                Write-Host "  > Renaming `"$($collection.Name)`" to `"$newName`""
+
+                if($WhatIf -eq $false)
+                {
+                    Set-CMDeviceCollection -CollectionId $collection.CollectionId -NewName "$newName" 
+                }
             }
 
         }
+
     }
+
+    Write-Progress -Id 1 -Activity "Processing collections in $FolderNodeName" -Status "$collectionCounter of $($folderCollections.Count)" -Completed
 
     if($Recurse)
     {
@@ -90,8 +132,8 @@ function Process-DeviceCollectionsInFolder
             -Filter "ParentContainerNodeID='$FolderNodeID' And ObjectType='5000'"
 
         foreach($subFolder in $allSubFolders)
-        {
-            Process-DeviceCollectionsInFolder -FolderNodeID $subFolder.ContainerNodeID -FolderNodeName "$FolderNodeName\$($subFolder.Name)"
+        { 
+            Process-DeviceCollectionsInFolder -FolderNodeID $subFolder.ContainerNodeID -FolderNodeName "$FolderNodeName\$($subFolder.Name)" -InsertPrefix:$InsertPrefix -ReplaceString:$ReplaceString
         }
     }   
 }
@@ -106,8 +148,8 @@ $providerNamespace = "root\sms\site_$siteCode"
 
 Push-Location $siteCode":"
 
-$topFolder = Get-WMIObject -Compute $providerSystem -Namespace $providerNamespace -Class SMS_ObjectContainerNode -Filter "Name='$Folder' And ObjectType='5000'"
+$topFolder = Get-WMIObject -Compute $providerSystem -Namespace $providerNamespace -Class SMS_ObjectContainerNode -Filter "Name='$FolderName' And ObjectType='5000'"
 
-Process-DeviceCollectionsInFolder -FolderNodeID $topFolder.ContainerNodeID -FolderNodeName $topFolder
+Process-DeviceCollectionsInFolder -FolderNodeID $topFolder.ContainerNodeID -FolderNodeName $topFolder.Name -InsertPrefix:($PSCmdlet.ParameterSetName -eq 'Prefix') -ReplaceString:($PSCmdlet.ParameterSetName -eq 'Replace')
 
 Pop-Location
