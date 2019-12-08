24f62c50cd694a2251c847678c71407a7635d18a
ConfigMgr Admin <cmadmin@lab300.configmgrftw.com>
2019-12-08 16:21:43 -0600

    Added Check-RegistryValueExists.ps1

---
+++ Other/Check-RegistryValueExists.ps1
@@ -0,0 +1,27 @@
+﻿param (
+
+ [parameter(Mandatory=$true)]
+ [string]$Path,
+
+[parameter(Mandatory=$true)]
+ [string]$Value
+)
+
+try {
+
+    if(Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null)
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+
+ }
+
+catch {
+
+return $false
+
+}
